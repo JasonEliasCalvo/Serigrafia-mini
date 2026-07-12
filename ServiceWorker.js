5e145ff990f3a79c4c431ddce59864fd1acbd18e
JasonCalvo <Jason_calvo@soy.sena.edu.co>
2026-07-12 19:45:39 -0300

    Final 7.0

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "DefaultCompany-Metaverso-0.1.0";
 const contentToCache = [
     "Build/cee3bfd5589651a8b16e2a12b8abe5b3.loader.js",
     "Build/6e52ad0ab2e86d80c0034b7af4ce28b7.framework.js.unityweb",
-    "Build/2b148b38bb34c774ebc4a3d2df89c49d.data.unityweb",
+    "Build/457ffd3dfe73e8a219700a1ebee7652b.data.unityweb",
     "Build/102f83738c060cb50ef3ff23d70fbd7a.wasm.unityweb",
     "TemplateData/style.css"
 
